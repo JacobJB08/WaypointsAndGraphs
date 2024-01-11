33ff20bb3ecf56b386d499705f3c404843541513
JacobJB08 <116194207+JacobJB08@users.noreply.github.com>
2024-01-11 13:49:33 -0800

    idek  :sob:

--- WaypointsAndGraphs/Assets/FollowWaypoint.cs
+++ WaypointsAndGraphs/Assets/FollowWaypoint.cs
@@ -8,24 +8,38 @@ public class FollowWaypoint : MonoBehaviour
     int currentWP = 0;
     public float speed = 10.0f;
     public float rotSpeed = 10.0f;
+    public float lookAhead = 10.0f;
+    GameObject tracker;
 
     void Start()
     {
-        
+        tracker = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        DestroyImmediate(tracker.GetComponent<Collider>());
+        tracker.GetComponent<MeshRenderer>().enabled = false;
+        tracker.transform.position = this.transform.position;
+        tracker.transform.rotation = this.transform.rotation;
     }
 
-    void Update()
+    void ProgressTracker()
     {
-        if (Vector3.Distance(this.transform.position, waypoints[currentWP].transform.position) < 2)
+        if (Vector3.Distance(tracker.transform.position, this.transform.position) > lookAhead) return;
+
+        if (Vector3.Distance(tracker.transform.position, waypoints[currentWP].transform.position) < 1)
             currentWP++;
 
         if (currentWP >= waypoints.Length)
             currentWP = 0;
 
-        //this.transform.LookAt(waypoints[currentWP].transform);
+        tracker.transform.LookAt(waypoints[currentWP].transform);
+        tracker.transform.Translate(0, 0, (speed + 60) * Time.deltaTime);
+    }
 
-        Quaternion lookatWP = Quaternion.LookRotation(waypoints[currentWP].transform.position - this.transform.position);
-        this.transform.rotation = Quaternion.Slerp(transform.rotation, lookatWP, rotSpeed * Time.deltaTime);
+    void Update()
+    {
+        ProgressTracker();
+
+        Quaternion lookatWP = Quaternion.LookRotation(tracker.transform.position - this.transform.position);
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, lookatWP, rotSpeed * Time.deltaTime);
         this.transform.Translate(0, 0, speed * Time.deltaTime);
     }
 }
